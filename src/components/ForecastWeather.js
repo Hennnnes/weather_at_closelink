c573815166a56ec0d7b897943c8200a646609b4d
hennnnes <hroemmer@googlemail.com>
2020-12-16 21:10:21 +0100

    FEAT: Show min/max temp for forecast

--- src/components/ForecastWeather.js
+++ src/components/ForecastWeather.js
@@ -3,11 +3,11 @@ import WeatherSet from './WeatherSet.js';
 const ForecastWeather = (props) => {
 	const icon = props.weather.weather[0].icon;
 	const description = props.weather.weather[0].description;
-	const temp = Math.round(props.weather.temp.day);
-	const feels_like = Math.round(props.weather.feels_like.day);
+	const min = Math.round(props.weather.temp.min);
+	const max = Math.round(props.weather.temp.max);
 
 	return (
-		<WeatherSet icon={icon} description={description} temp={temp} feels_like={feels_like} />
+		<WeatherSet icon={icon} description={description} min={min} max={max} />
 	)
 }
 
--- src/components/WeatherSet.js
+++ src/components/WeatherSet.js
@@ -1,13 +1,27 @@
 import './WeatherSet.css';
 
 const WeatherSet = (props) => {
-	return (
-		<div>
-          	<img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt={props.description} />
-          	<p>
-		  		<span>{props.temp}°C, </span>
+	let content = '';
+
+	if (props.temp && props.feels_like) {
+		content = (
+			<p className="weatherset__contents">
+				<span>{props.temp}°C, </span>
 				<span>feels like {props.feels_like}°C</span>
 			</p>
+		)
+	} else if (props.min && props.max) {
+		content = (
+			<p className="weatherset__contents">
+				<span>min: {props.min}°C, </span>
+				<span>max: {props.max}°C</span>
+			</p>
+		)
+	}
+	return (
+		<div className="weatherset">
+			<img src={`http://openweathermap.org/img/wn/${props.icon}@4x.png`} alt={props.description} className="weatherset__image"/>
+			{content}
 		</div>
 	)
 }
